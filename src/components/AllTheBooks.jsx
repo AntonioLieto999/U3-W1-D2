5373070bad407f2f0d17f0e35ae139bee237fc1e
Antonio Lieto <antonio.lieto1999@gmail.com>
2025-05-13 15:06:37 +0200

    aggiunta di bottoni che cambiano la categoria di libri

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,12 +1,36 @@
-import fantasy from "../data/fantasy.json";
+import { useState } from "react";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
+import fantasy from "../data/fantasy.json";
+import horror from "../data/horror.json";
+import romance from "../data/romance.json";
+import scifi from "../data/scifi.json";
+import history from "../data/history.json";
 
 const AllTheBooks = function () {
+  const [books, setBooks] = useState(fantasy); // Stato iniziale con i libri fantasy
+
   return (
     <Container className="my-4">
-      <h2 className="text-center mb-4">Libri Fantasy</h2>
+      <h2 className="text-center mb-4">Libri</h2>
+      <div className="d-flex justify-content-center mb-4">
+        <Button className="mx-2" onClick={() => setBooks(history)}>
+          history
+        </Button>
+        <Button className="mx-2" onClick={() => setBooks(scifi)}>
+          Scifi
+        </Button>
+        <Button className="mx-2" onClick={() => setBooks(fantasy)}>
+          Fantasy
+        </Button>
+        <Button className="mx-2" onClick={() => setBooks(horror)}>
+          Horror
+        </Button>
+        <Button className="mx-2" onClick={() => setBooks(romance)}>
+          Romance
+        </Button>
+      </div>
       <Row>
-        {fantasy.slice(0, 15).map((book) => (
+        {books.slice(0, 15).map((book) => (
           <Col key={book.asin} md={4} className="mb-4">
             <Card className="h-100">
               <Card.Img variant="top" src={book.img} alt={book.title} />
